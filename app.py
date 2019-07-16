405fd5c9aa31fd7790df34604ffcbc88e0c9f747
raaj <raajtilaksarma@gmail.com>
2019-07-17 00:34:38 +0530

    get removed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST','GET'])
+@app.route("/", methods=['POST'])
 def index():
     counts = []
     if request.method=='POST':
