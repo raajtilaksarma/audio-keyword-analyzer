37a052384895ed4a82879bf75f363db039bebf91
raaj <raajtilaksarma@gmail.com>
2019-07-17 01:56:59 +0530

    a

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def index():
     counts = []
     if request.method=='POST':
-        f = request.files['audiofile']
-        f.save(secure_filename(f.filename))
-        audioFileName = f.filename
-        f = request.files['keywordfile']
-        f.save(secure_filename(f.filename))
-        keywordFileName = f.filename
-        #kw = KeywordCounter(audioFileName,keywordFileName)
-        #counts = kw.getResult()
-        print(counts)
+        # f = request.files['audiofile']
+        # f.save(secure_filename(f.filename))
+        # audioFileName = f.filename
+        # f = request.files['keywordfile']
+        # f.save(secure_filename(f.filename))
+        # keywordFileName = f.filename
+        # #kw = KeywordCounter(audioFileName,keywordFileName)
+        # #counts = kw.getResult()
+        # print(counts)
         return render_template('index.html',counts=counts)
 
 if __name__ == "__main__":
